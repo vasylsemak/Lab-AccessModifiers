d422455c3dcee1398ff95a0a67ad92f2c58cfad1
Vasyl Semak <vasylsmk1401@gmail.com>
2021-06-28 13:24:18 -0400

    Learn how to control access for class state and behavior using access modifiers

---
+++ src/AccessModifiers.java
@@ -0,0 +1,11 @@
+
+public class AccessModifiers {
+
+	public static void main(String[] args) {
+		Person adam = new Person();
+		adam.age = 25;
+		
+		System.out.println("Age: " + adam.age);
+	}
+
+}
